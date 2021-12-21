8b67167485d9f637193b0b7b7bcbb747f2bed6a4
howell <79634945+willwho08@users.noreply.github.com>
2021-12-21 11:41:03 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,6 +109,24 @@ function stopPrntScr() {
         setInterval("AccessClipboardData()", 300);
 
 
+/** TO DISABLE SCREEN CAPTURE **/
+document.addEventListener('keyup', (e) => {
+    if (e.key == 'PrintScreen') {
+        navigator.clipboard.writeText('');
+        alert('Screenshots disabled!');
+    }
+});
+
+/** TO DISABLE PRINTS WHIT CTRL+P **/
+document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key == 'p') {
+        alert('This section is not allowed to print or export to PDF');
+        e.cancelBubble = true;
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    }
+});
+
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
